04b759e488bfbfc8155de264961a6d66df756013
smluo <1374497925@qq.com>
2021-05-30 23:52:02 +0800

    this指向问题

--- js/spider.js
+++ js/spider.js
@@ -7,17 +7,21 @@ const vm = new Vue({
       data:[1,2,3,4,5,6,7,8,9]
     };
   },
-  methods: {},
+  methods: {
+    searchFn: function() {
+      let value = document.querySelector('#value').value;
+      let that = this;
+      axios
+        .get(`http://localhost:8080/api/highlight/${value}/1/10`)
+        .then(function (response) {
+          let res = response.data;
+          that.books = res;
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+    }
+  },
   mounted() {
-    let that = this;
-    axios
-      .get("http://localhost:8080/api/highlight/hello/1/10")
-      .then(function (response) {
-        let res = response.data;
-        that.books = res;
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
   },
 });
